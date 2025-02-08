cf418cd4658a9796e7ff9e680a8c359316beaaa4
Victor Filgueira <victor.filgueira@mindsatwork.com.br>
2025-02-07 23:16:39 -0300

    feat: add programs list

---
+++ src/app/components/programs/programs.component.ts
@@ -0,0 +1,61 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+interface ProgramCard {
+  id: number;
+  name: string;
+  imageUrl: string;
+  text: string;
+  isStarted: boolean;
+}
+
+@Component({
+  selector: 'app-programs',
+  templateUrl: './programs.component.html',
+  styleUrls: ['./programs.component.scss'],
+  imports: [CommonModule, IonicModule],
+})
+export class ProgramsComponent implements OnInit {
+  cards: ProgramCard[] = [
+    {
+      id: 1,
+      name: 'Programa Iniciante',
+      imageUrl: '../../../assets/images/lift.jpg',
+      text: 'Começar Programa',
+      isStarted: false,
+    },
+    {
+      id: 2,
+      name: 'Programa Intermediário',
+      imageUrl: '../../../assets/images/yoga.jpg',
+      text: 'Começar Programa',
+      isStarted: false,
+    },
+    {
+      id: 3,
+      name: 'Programa Avançado',
+      imageUrl: '../../../assets/images/spinning.jpg',
+      text: 'Começar Programa',
+      isStarted: false,
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.loadPrograms();
+  }
+
+  loadPrograms(): void {
+    // Aqui você pode adicionar a lógica para carregar os programas
+  }
+
+  toggleProgramStart(cardId: number): void {
+    const card = this.cards.find((c) => c.id === cardId);
+    if (card) {
+      card.isStarted = !card.isStarted;
+      card.text = card.isStarted ? 'Continuar Programa' : 'Começar Programa';
+    }
+  }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,11 +3,18 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { HeaderComponent } from '../components/header/header.component';
 import { OnlineCoachComponent } from '../components/online-coach/online-coach.component';
+import { ProgramsComponent } from '../components/programs/programs.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
-  imports: [CommonModule, IonicModule, HeaderComponent, OnlineCoachComponent],
+  imports: [
+    CommonModule,
+    IonicModule,
+    HeaderComponent,
+    OnlineCoachComponent,
+    ProgramsComponent,
+  ],
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage {}
